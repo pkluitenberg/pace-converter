5001bead068f5732c1e89c0624e3d2fc840cc4a0
Paul Kluitenberg <paul.kluitenberg@gmail.com>
2022-11-10 18:43:08 +0100

    add in convert icon

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from './Components/Header';
 import Dropdown from './Components/Dropdown';
 import Box from '@mui/material/Box';
 import Autorenew from '@mui/icons-material/Autorenew';
+import ForwardOutlinedIcon from '@mui/icons-material/ForwardOutlined';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
@@ -16,12 +17,12 @@ function App() {
     const secondsValues = Array.from(Array(60).keys())
     const unitsValues = ['mins/km', 'mins/mi']
 
-    const [seconds, setSeconds] = React.useState(0);
-    const [minutes, setMinutes] = React.useState(0);
+    const [seconds, setSeconds] = React.useState('');
+    const [minutes, setMinutes] = React.useState('');
     const [output, setOutput] = React.useState('');
     const [unitsFrom, setUnitsFrom] = React.useState(unitsValues[0]);
     const [unitsTo, setUnitsTo] = React.useState(unitsValues[1]);
-    
+
     const handleClick = (event) => {
         const convertedSeconds = paceConverter(minutes, seconds, unitsFrom);
         const displayMinutes = Math.floor(convertedSeconds / 60)
@@ -64,7 +65,7 @@ function App() {
             </Box>
             <Box sx={boxFormat}>
                 <Grid container spacing={1}>
-                    <Grid item sm={6} >
+                    <Grid item sm={5} >
                         <Dropdown
                             label='From'
                             values={unitsValues}
@@ -72,7 +73,10 @@ function App() {
                             value={unitsFrom}
                         />
                     </Grid>
-                    <Grid item sm={6}>
+                    <Grid item sm={2} alignItems="center" justifyContent="center" display="flex">
+                        <ForwardOutlinedIcon fontSize="large" color="action"></ForwardOutlinedIcon>
+                    </Grid>
+                    <Grid item sm={5}>
                         <Dropdown
                             label='To'
                             values={unitsValues}
